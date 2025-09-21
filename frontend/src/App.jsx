e8abb46072da75e948e86c5d35e65851b5c7e83f
Mithun Chanderan <mithunchanderan@gmail.com>
2025-09-21 15:44:43 +0530

    CASESENSITIVE CHANGED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // client/src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/Loginpage';
 import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 import TeacherDashboard from './pages/TeacherDashboard';
