d6322c3f69b4a4d496b60f504109ff40b56d38e1
Mithun Chanderan <mithunchanderan@gmail.com>
2025-09-21 15:07:50 +0530

    FILES CHANGES MADE

--- backend/models/Assignments.js
+++ backend/models/Assignments.js
@@ -4,7 +4,6 @@ const assignmentSchema = new mongoose.Schema({
   title: { type: String, required: true },
   subject: { type: String, required: true },
   desc: { type: String, required: true },
-  // FIX: Change ref to "User" to match the User model
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   assignedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 }, { timestamps: true });
